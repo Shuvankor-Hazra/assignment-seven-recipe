6872643a18853458bf7aecb53ee14e5e27f6caa1
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-03-15 20:59:46 +0600

    Assignment 7 all setup ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-9xl text-violet-400">Assignment - 7</h1>
+
+      <button className="btn btn-accent">Accent</button>
+    </>
+  );
+}
+
+export default App;
