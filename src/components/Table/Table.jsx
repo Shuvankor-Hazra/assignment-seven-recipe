43039fcfefcf29230053f77c2458fa4f26ea65ce
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-03-16 15:10:55 +0600

    Empty table fixed

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,44 +1,41 @@
-
-
 const Table = () => {
-    return (
-        <div className="pt-5">
-  <table className="text-left">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>SL</th>
-        <th>Name</th>
-        <th>Time</th>
-        <th>Calories</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="border-y">
-        <th>1</th>
-        <td>Spaghetti Bolognese</td>
-        <td>20 Minutes</td>
-        <td>600 Calories</td>
-        <td className="btn bg-[#A5DD9B] rounded-3xl">Ready</td>
-      </tr>
-      <tr className="border-y">
-        <th>2</th>
-        <td>Spaghetti Bolognese</td>
-        <td>20 Minutes</td>
-        <td>600 Calories</td>
-        <td className="btn bg-[#A5DD9B] rounded-3xl">Ready</td>
-      </tr>
-      <tr className="border-y">
-        <th>3</th>
-        <td>Spaghetti Bolognese</td>
-        <td>20 Minutes</td>
-        <td>600 Calories</td>
-        <td className="btn bg-[#A5DD9B] rounded-3xl">Ready</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-    );
+  return (
+    <div className=" pt-5">
+      <div className="text-left">
+        <div>
+          <tr className="w-[70%] flex justify-between">
+            <th>SL</th>
+            <th>Name</th>
+            <th>Time</th>
+            <th>Calories</th>
+          </tr>
+        </div>
+        <div>
+          <tr className="border-y-2">
+            <th>1</th>
+            <td>Spaghetti Bolognese</td>
+            <td>20 Minutes</td>
+            <td>600 Calories</td>
+            <td className="btn bg-[#A5DD9B] rounded-3xl">Ready</td>
+          </tr>
+          <tr className="border-y">
+            <th>2</th>
+            <td>Spaghetti Bolognese</td>
+            <td>20 Minutes</td>
+            <td>600 Calories</td>
+            <td className="btn bg-[#A5DD9B] rounded-3xl">Ready</td>
+          </tr>
+          <tr className="border-y">
+            <th>3</th>
+            <td>Spaghetti Bolognese</td>
+            <td>20 Minutes</td>
+            <td>600 Calories</td>
+            <td className="btn bg-[#A5DD9B] rounded-3xl">Ready</td>
+          </tr>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
